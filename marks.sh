5921eb8791469e26df056240424deb6509f854c9
mondus <p.richmond@sheffield.ac.uk>
2017-01-27 09:43:24 +0000

    queue name

--- marks.sh
+++ marks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Request GPU resource
 #$ -l gpu=1
-# Use the flybrain project group
-#$ -P flybrain
-#Use the flybrain queue
-#$ -q flybrain.q 
+# Use the training project group
+#$ -P training
+#Use the training queue
+#$ -q training.q 
 
 # Call your CUDA executable
 ./marks
